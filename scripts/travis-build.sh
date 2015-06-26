81b3300f8cf683e56d3c531040bdebb70c1f8545
Joe Schafer <joe@jschaf.com>
2015-06-25 22:25:07 -0400

    fix(build): remove call to travis-skip.py

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-python scripts/travis_skip.py
-
-if [ "$?" -eq "0" ]
-then
-    coverage run test/test_func.py
-else
-    echo "Skipping"
-fi
+coverage run test/test_func.py
--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-python scripts/travis_skip.py
-
-if [ "$?" -eq "0" ]
-then
-    pip install --use-mirrors coverage coveralls
-    pip install --use-mirrors werkzeug
-    pip install --use-mirrors git+https://github.com/landscapeio/pylint-plugin-utils.git@develop
-    pip install --use-mirrors --editable .
-else
-    echo "Skipping"
-fi
+pip install --use-mirrors coverage coveralls
+pip install --use-mirrors werkzeug
+pip install --use-mirrors git+https://github.com/landscapeio/pylint-plugin-utils.git@develop
+pip install --use-mirrors --editable .
